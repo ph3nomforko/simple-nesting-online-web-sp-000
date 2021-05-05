ce339ba299d966638f498e691648ca193b809024
Codi Kraus <codi.kraus@gmail.com>
2021-05-05 16:26:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -75,10 +75,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-	programmer_hash[:yukihiro_matsumoto] = {}
-	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-	programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-
+	programmer_hash[:yukihiro_matsumoto] = {
+		:known_for = "Ruby",
+		:languages = ["LISP", "C"]
+	}
 end
 
 def changing_alan
