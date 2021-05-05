a3be0b8c1a6257fb1bf9b8ece2c4ef6d0eb32842
Codi Kraus <codi.kraus@gmail.com>
2021-05-05 16:31:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -95,8 +95,6 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
 	programmer_hash[:alan_kay][:known_for] = "GUI"
 	programmer_hash
 end
@@ -117,5 +115,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+	programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
